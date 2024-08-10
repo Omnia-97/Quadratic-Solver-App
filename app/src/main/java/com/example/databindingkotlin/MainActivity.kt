5f04226b0bf0ba8f4abe11b7c2471b6ab1fd917e
DELL <omnianabil1920@gmail.com>
2024-08-11 00:01:29 +0300

    finished app

--- app/src/main/java/com/example/databindingkotlin/MainActivity.kt
+++ app/src/main/java/com/example/databindingkotlin/MainActivity.kt
@@ -7,12 +7,14 @@ import androidx.databinding.DataBindingUtil
 import com.example.databindingkotlin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-       // setContentView(R.layout.activity_main)
-        binding = DataBindingUtil.setContentView(this , R.layout.activity_main)
-        var textView : TextView = findViewById(R.id.textView)
-        textView.text = "Hello text !"
+        var mainBinding = DataBindingUtil.setContentView<ActivityMainBinding>(
+            this,
+            R.layout.activity_main
+        )
+        var myEquation = MyEquation(mainBinding)
+        mainBinding.equation = myEquation
+
     }
 }
--- app/src/main/java/com/example/databindingkotlin/MyEquation.kt
+++ app/src/main/java/com/example/databindingkotlin/MyEquation.kt
@@ -1,13 +1,53 @@
 package com.example.databindingkotlin
 
+import android.view.View
 import androidx.databinding.BaseObservable
 import androidx.databinding.Bindable
 import com.example.databindingkotlin.databinding.ActivityMainBinding
+import kotlin.math.sqrt
 
-class MyEquation : BaseObservable() {
+class MyEquation(var binding: ActivityMainBinding) : BaseObservable() {
     @Bindable
-    var a : String = ""
-    var b : String = ""
-    var c : String = ""
-    lateinit var binding : ActivityMainBinding
+    var a: String = ""
+        set(value) {
+            field = value
+            notifyPropertyChanged(BR.a)
+        }
+
+    @Bindable
+    var b: String = ""
+        set(value) {
+            field = value
+            notifyPropertyChanged(BR.b)
+        }
+
+    @Bindable
+    var c: String = ""
+        set(value) {
+            field = value
+            notifyPropertyChanged(BR.c)
+        }
+
+    fun solveEquation(view: View) {
+        val a = a.toInt()
+        val b = b.toInt()
+        val c = c.toInt()
+        val discriminant = ((b * b) - (4 * a * c))
+        val x1: Double
+        val x2: Double
+        if (discriminant > 0) {
+            x1 = (-b + sqrt(discriminant.toDouble())) / (2 * a)
+            x2 = (-b - sqrt(discriminant.toDouble())) / (2 * a)
+            binding.result.text = "X1 = $x1 \nX2 = $x2"
+        }else if (discriminant < 0){
+            binding.result.text = "No real roots"
+        }else{
+            x1 = (-b / (2 * a).toDouble())
+            binding.result.text = "X = $x1"
+        }
+
+
+    }
+
+
 }
